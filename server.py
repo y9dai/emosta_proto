3769bf9053e43fa2561097a7fd0b201e9157b362
y9nagie <y9nagie@gmail.com>
2019-07-29 00:18:54 +0900

    add img2poem

--- server.py
+++ server.py
@@ -1,49 +1,63 @@
-from flask import Flask, render_template, request, redirect, url_for, send_from_directory
+from flask import Flask, render_template, request, redirect, url_for, send_from_directory, json, jsonify
+from datetime import datetime
+from translate import Translator
 import numpy as np
 import cv2
-from image_process import canny
-from datetime import datetime
 import os
-import string
 import random
+import time
+import string
+import shutil
+import sys
+sys.path.append('src')
+import nn_process
+
 
 SAVE_DIR = "./images"
-if not os.path.isdir(SAVE_DIR):
-    os.mkdir(SAVE_DIR)
+translator = Translator(from_lang = "en", to_lang = "ja")
 
 app = Flask(__name__, static_url_path="")
+
 
 def random_str(n):
     return ''.join([random.choice(string.ascii_letters + string.digits) for i in range(n)])
 
+
 @app.route('/')
 def index():
-    return render_template('index.html', images=os.listdir(SAVE_DIR)[::-1])
+    global extract_feature
+    global generate_poem
+    extract_feature = nn_process.create('extract_feature')
+    generate_poem = nn_process.create('generate_poem')
+    return render_template('index.html')
 
 @app.route('/images/<path:path>')
 def send_js(path):
     return send_from_directory(SAVE_DIR, path)
 
-# 参考: https://qiita.com/yuuuu3/items/6e4206fdc8c83747544b
 @app.route('/upload', methods=['POST'])
 def upload():
+    shutil.rmtree(SAVE_DIR)
+    os.mkdir(SAVE_DIR)
+
     if request.files['image']:
         # 画像として読み込み
         stream = request.files['image'].stream
         img_array = np.asarray(bytearray(stream.read()), dtype=np.uint8)
         img = cv2.imdecode(img_array, 1)
 
-        # 変換
-        img = canny(img)
-
         # 保存
         dt_now = datetime.now().strftime("%Y_%m_%d%_H_%M_%S_") + random_str(5)
-        save_path = os.path.join(SAVE_DIR, dt_now + ".png")
+        save_path = os.path.join(SAVE_DIR, dt_now + ".jpg")
         cv2.imwrite(save_path, img)
 
-        print("save", save_path)
+        # img2poem
+        img_feature = extract_feature(save_path)
+        poems = generate_poem(img_feature)[0].split('\n')
+        for i in range(len(poems)):
+            poems[i] = translator.translate(poems[i])
 
-        return redirect('/')
+        return render_template('index.html', images=os.listdir(SAVE_DIR)[::-1], poems=poems)
 
 if __name__ == '__main__':
     app.debug = True
